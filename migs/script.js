8d71fc5724389bb1c5248a70ab971e07454de0da
Migspog12 <elmigu366@gmail.com>
2025-09-06 10:33:55 -0300

    Add files via upload

---
+++ migs/script.js
@@ -0,0 +1,39 @@
+/*
+alert('hello world')
+document.writeln('bom dia')
+console.log('bom dia')
+*/
+
+nome = 'games'
+cidade = 'dava jones'
+
+
+
+document.writeln('<h1>Aula de JS</h1>')
+//document.writeln('me chamo '+nome+' e moro em '+cidade)
+
+
+a = 1
+b = 2
+
+
+soma = a+b
+menos = a-b
+multi = a*b
+divisao = a/b
+
+document.writeln('Adição:'+soma)
+document.writeln('<br>Subtração:'+menos)
+document.writeln('<br>multi:'+multi)
+document.writeln('<br>divisão:'+divisao)
+
+
+document.writeln(
+    '<br>adicao:'+soma+
+    '<br>subtracao'+menos+
+    '<br>multi:'+multi+
+    '<br>divisao:'+divisao
+)
+
+
+
