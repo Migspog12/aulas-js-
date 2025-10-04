ba201a6c8552a7b8a0492cfd5dc5494be610660a
Migspog12 <elmigu366@gmail.com>
2025-10-04 10:32:00 -0300

    Add files via upload

---
+++ migs/aula3.js
@@ -0,0 +1,13 @@
+function negocio(){
+numero = Number(document.getElementById('n1').value)
+
+
+
+if( numero > 0 ){
+    alert('Positivo')
+}else if( numero < 0 ){
+    alert('Negativo')
+}else{
+    alert('N/A')
+}
+}
